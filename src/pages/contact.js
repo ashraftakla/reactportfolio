3f4ea9cd37121980f26e20c0e2017e02ceb25b8f
Ashraf Takla <ashraf.takla@icloud.com>
2021-01-05 11:45:49 -0500

    initial react app setup

---
+++ src/pages/contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+function contact() {
+    return (
+        <div>
+            <h1> Contact Me</h1>
+            <p>
+                Tel: 703-663-0041
+<br></br>
+Email: ashraf.takla@icloud.com
+<a href="mailto:ashraf.takla@icloud.com" class="btn"><button id="emailbtn">Email Me</button></a>
+                <br></br>
+Github: ashraftakla
+            </p>
+        </div>
+    )
+}
+export default contact
